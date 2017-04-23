540b405e01e1ac4abfefea45bd00f3b5a52a0ac7
Jelly <Jelly>
2017-04-23 18:03:22 +0200

    Fix [FE] Logo visibility fixed

--- js/controllers/NavbarController.js
+++ js/controllers/NavbarController.js
@@ -1,10 +1,5 @@
-angular.module("emistiApp").controller("NavbarController", ["$rootScope", "$scope", function($rootScope, $scope) {
-  $scope.isLogoVisible = false;
-
-  $rootScope.$on('$stateChangeStart',
-    function(event, toState, toParams, fromState, fromParams) {
-      if (toState.name != "home") {
-        $scope.isLogoVisible = true;
-      }
-    });
+angular.module("emistiApp").controller("NavbarController", ["$scope", "$state", function($scope, $state) {
+  $scope.isLogoVisible = function() {
+    return $state.current && $state.current.name != "home" && $state.current.name != "";
+  };
 }]);
