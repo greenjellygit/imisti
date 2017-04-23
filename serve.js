401dd4c35fb5779b0a75ac05b3bf635b8f2f2e42
Jelly <Jelly>
2017-04-23 22:42:16 +0200

    Fix [BE] FB integration on openshift

--- serve.js
+++ serve.js
@@ -1,8 +1,8 @@
 var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
-var fb = require('fb');
 var util = require('util');
+var unirest = require('unirest')
 
 var app = express();
 
@@ -21,28 +21,42 @@ app.get('/', function(req, res) {
 var pageId = "284449561997051";
 var accessToken = null;
 
-fb.api('oauth/access_token', {
-  client_id: '463956137276928',
-  client_secret: 'b6dbb81347dfa93bbf63470f064c1fae',
-  grant_type: 'client_credentials'
-}, function(res) {
-  if (!res || res.error) {
-    console.log(!res ? 'error occurred' : res.error);
-    return;
-  }
-  accessToken = res.access_token;
-  util.log("retreived access_token: " + accessToken);
-});
+var urls = {
+  appAccesToken: "https://graph.facebook.com/oauth/access_token",
+  fanpagePosts: "https://graph.facebook.com/" + pageId + "/posts"
+};
 
-app.get('/getNews', function(req, res) {
-  util.log("REST: getNews() called");
-
-  fb.api(pageId + "/posts?access_token=" + accessToken,
-    function(response) {
-      res.json(response.data);
+util.log('GET request: FB api access_token');
+unirest.get(urls.appAccesToken)
+  .query({
+    client_id: '463956137276928',
+    client_secret: 'b6dbb81347dfa93bbf63470f064c1fae',
+    grant_type: 'client_credentials'
+  })
+  .end(function(res) {
+    if (res.error) {
+      console.log('GET error', res.error)
+    } else {
+      accessToken = res.body.access_token;
+      util.log("GET response: Retreived access_token: " + accessToken);
     }
-  );
+  });
 
+app.get('/getNews', function(req, response) {
+  util.log('GET request: getNews');
+  unirest.get(urls.fanpagePosts)
+    .query({
+      access_token: accessToken
+    })
+    .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+    .end(function(res) {
+      if (res.error) {
+        console.log('GET error', res.error)
+      } else {
+        util.log("GET response: getNews ");
+        response.json(res.body.data);
+      }
+    });
 });
 
 http.createServer(app).listen(app.get('port'), app.get('ip'), function() {
